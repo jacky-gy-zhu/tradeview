15725bd91ec3bbbf7bfddfa93180baa5d157e376
jacky-gy-zhu <jacky.gy.zhu@gmail.com>
2021-01-11 19:56:58 +1100

    update

--- src/main/java/com/tradeview/stock/config/Param.java
+++ src/main/java/com/tradeview/stock/config/Param.java
@@ -39,9 +39,9 @@ public class Param {
 	// N字型走势回调最小幅度(MA5)
 	public static float N_BACK_MA5_MIN_RATE = 0.01f;
 	// N字型走势回调比例最大值
-	public static float N_BACK_BODY_MAX_RATE = 0.36f;
+	public static float N_BACK_BODY_MAX_RATE = 0.382f;
 	// N字型走势回调比例最小值
-	public static float N_BACK_BODY_MIN_RATE = 0.22f;
+	public static float N_BACK_BODY_MIN_RATE = 0.236f;
 	// 调整时间最大值
 	public static int N_BACK_MIN_DAYS = 10;
 }
