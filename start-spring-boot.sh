e1e134ee398fdc163e8eb478ed0efbad26a3d2c4
jacky-gy-zhu <jacky.gy.zhu@gmail.com>
2021-01-16 21:39:44 +1100

    update

--- start-spring-boot.sh
+++ start-spring-boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup java -jar tradeview-1.0.jar >tradeview.log &
+nohup java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8001 -jar tradeview-1.0.jar >tradeview.log &
